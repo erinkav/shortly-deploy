17438acd6adc35579e54c19d84135d10d68d936f
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 17:32:13 -0700

    Mongoose refactor to add User and Link databases

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,30 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose'); 
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+}); 
+
+userSchema.post('save', function () {
+  this.hashPassword(); 
 });
 
-module.exports = User;
+userSchema.methods.comparePassword = function(attemptedPassword, callback) { //refactor
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+userSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      console.log('the hash: ' + this.password);  
+    });
+};
+
+module.exports = userSchema;
