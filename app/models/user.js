004aa52b2e85a7744db00969a7129801fc943104
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 20:10:50 -0700

    Continue mongo refactor

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true},
@@ -12,18 +12,21 @@ userSchema.post('save', function () {
   this.hashPassword(); 
 });
 
-userSchema.methods.comparePassword = function(attemptedPassword, callback) { //refactor
-  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  var user = this; 
+  bcrypt.compare(attemptedPassword, user.password, function(err, isMatch) {
+    console.log( 'this.password in ', user.password); 
     callback(isMatch);
   });
 };
 
 userSchema.methods.hashPassword = function() {
+  var user = this; 
   var cipher = Promise.promisify(bcrypt.hash);
   return cipher(this.password, null, null).bind(this)
     .then(function(hash) {
-      this.password = hash;
-      console.log('the hash: ' + this.password);  
+      user.password = hash;
+      console.log('the hash: ' + user.password);  
     });
 };
 
